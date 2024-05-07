638cd8ddcd49dc87c91a9329748c0021ca2a6c3b
pavel-cpp <toruchan174@gmail.com>
2024-05-07 23:12:14 +0300

    Create a base to get started

---
+++ mime_node.cpp
@@ -0,0 +1,8 @@
+//
+// Created by green on 07-May-24.
+//
+
+#include "mime_node.h"
+
+namespace magic {
+} // magic
---
+++ mime_node.h
@@ -0,0 +1,12 @@
+#ifndef MIME_NODE_H
+#define MIME_NODE_H
+
+namespace magic {
+
+class mime_node {
+
+};
+
+} // magic
+
+#endif //MIME_NODE_H
