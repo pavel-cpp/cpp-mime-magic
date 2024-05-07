0f5c9ef3636097333a858cc5e0319735a1821c25
pavel-cpp <toruchan174@gmail.com>
2024-05-07 23:31:54 +0300

    Сделай объявления класса mime

--- mime_node.cpp
+++ mime_node.cpp
@@ -1,8 +1,5 @@
-//
-// Created by green on 07-May-24.
-//
-
 #include "mime_node.h"
 
 namespace magic {
+
 } // magic
--- mime_node.h
+++ mime_node.h
@@ -1,12 +1,32 @@
 #ifndef MIME_NODE_H
 #define MIME_NODE_H
 
+#include <cstdint>
+#include <string>
+#include <variant>
+#include <vector>
+
 namespace magic {
+    class mime_node;
 
-class mime_node {
+    using array = std::vector<mime_node>;
 
-};
+    class mime_node
+        : private std::variant<
+            std::nullptr_t,
+            std::pair<uint8_t, array>,
+            std::pair<uint16_t, array>,
+            std::pair<uint32_t, array>,
+            std::pair<uint64_t, array>,
+            std::pair<std::string, array>
+    >{
+    public:
+        using variant::variant;
+        using value = variant;
 
+        template<typename input_it>
+        bool process_data(input_it begin, input_it end);
+    };
 } // magic
 
 #endif //MIME_NODE_H
